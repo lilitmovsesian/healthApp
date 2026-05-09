2e45f128582822509c95e8f3419716821b399408
lilitmovsesian <lilitmovsesian@proton.me>
2026-05-10 02:25:59 +0300

    first commit

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+from langchain.chat_models import init_chat_model
+from pypdf import PdfReader
+
 def main():
-    print('A')
+    model = init_chat_model("deepseek-v4-flash")
+    #resp = model.invoke("Hello")
+    #print(resp)
+    reader = PdfReader("docs/sample_1.pdf")
+    for page in reader.pages:
+        print(page.extract_text())
 
 if __name__=="__main__":
     main()
